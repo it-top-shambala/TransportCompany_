f5a28870d04fb054c1be92d8a43521591d3de130
Starinin Andrey <starininandrey@gmail.com>
2023-07-26 21:34:52 +0300

    Testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+#include "./lib/Order.h"
+#include "./lib/CargoTransport.h"
+
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    auto truck = new CargoTransport("KAMAZ", TransportType::Car, 5000);
+    auto order = new Order(truck, "point 1", "point 2");
+
+    auto transportTemp = order->getTransport();
+    cout << order->getStart() << " -> " << order->getFinish() << transportTemp->getName() << endl;
+    cout << "objects = " << transportTemp->getObjects()->size() << endl;
+    delete transportTemp;
+
+    truck->addObject(new Cargo("cargo", 4000));
+
+    auto transportTemp2 = order->getTransport();
+    cout << "objects = " << transportTemp2->getObjects()->size() << endl;
+    cout << "object = " << dynamic_cast<Cargo*>(transportTemp2->getObjects()->at(0))->getName() << endl;
+    delete transportTemp2;
+
     return 0;
 }
